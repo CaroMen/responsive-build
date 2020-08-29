7e33827fc37e2d22a9b8db235f07e38d7b4d78a4
Caroline <caroline_mendez@baylor.edu>
2020-08-29 17:03:15 -0500

    Main page

---
+++ src/components/MainPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <section id="welcome" class="grid">
+      <div class="welcome-text">
+        <h2>Developer</h2>
+        <p
+          class="l"
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae nostrum necessitatibus repudiandae facilis tempore.</p>
+        <a href="#" class="button">View my work</a>
+      </div>
+    </section>
+    <section id="portfolio">
+      <h3>Some of my Projects</h3>
+      <div class="projects grid">
+        <a href>
+          <img :src="require('@/assets/shaka.png')" alt />
+          <h4>Sorting Visualizer</h4>
+        </a>
+        <a href>
+          <img :src="require('@/assets/temp-photo.jpeg')" alt />
+          <h4>Moxi Web</h4>
+        </a>
+        <a href>
+          <img :src="require('@/assets/temp-photo-2.jpeg')" alt />
+          <h4>Insta Clone</h4>
+        </a>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    // photo1: require("@/assets/shaka.png"),
+  }),
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended z-depth-0">
+      <div class="nav-content">
+        <router-link to>
+          <span class="nav-title text">Caro.</span>
+        </router-link>
+        <router-link to>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <router-link :to="{ name: '#' }">
+                <a>Home</a>
+              </router-link>
+            </li>
+            <li>
+              <router-link :to="{ name: '#' }">
+                <a>Portfolio</a>
+              </router-link>
+            </li>
+            <li>
+              <router-link active-class="active" :to="{ name: '#' }">
+                <a>Contact</a>
+              </router-link>
+            </li>
+          </ul>
+        </router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #f4f4f5;
+  color: #c6a277;
+}
+
+.text {
+  color: #c6a277;
+}
+
+.navbar nav {
+  padding: 0 60px;
+  font-family: Lato, Helvetica, Arial, sans-serif;
+  background-color: #f4f4f5;
+}
+
+.nav-title {
+  color: #48564f;
+  font-size: 50px;
+}
+
+#nav-mobile {
+  padding: 30px 10px;
+  color: #c6a277;
+}
+
+/* iPad */
+@media (min-device-width: 768px) and (max-device-width: 1024px) {
+  .navbar nav {
+    padding: 0 60px;
+    font-family: Lato, Helvetica, Arial, sans-serif;
+  }
+  #nav-mobile {
+    margin-top: -255px;
+    padding: 90px 70px;
+    margin-left: -100px;
+  }
+}
+
+/* iPad Pro */
+@media (min-device-width: 1024px) and (max-device-width: 1366px) and (-webkit-min-device-pixel-ratio: 2) {
+  .navbar nav {
+    padding: 0 60px;
+    font-family: Lato, Helvetica, Arial, sans-serif;
+  }
+  #nav-mobile {
+    margin-top: -55px;
+    padding: 90px 70px;
+  }
+}
+</style>
